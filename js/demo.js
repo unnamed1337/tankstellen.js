62d387cb791695d4715e7cace119ec00a3b9f522
Pascal <dev@pascal-gollnick.de>
2018-07-22 00:25:02 +0200

    Vorbereiten Demo

--- js/demo.js
+++ js/demo.js
@@ -1,3 +1,14 @@
 /*  
   TODO: Create Demo
 */
+
+$(function(){
+  tankstellen.Init({
+    apiKey:"00000000-0000-0000-0000-000000000002"
+  });
+  
+  
+  tankstellen.Finden({
+    sortBy:"dist"
+  });
+});
--- js/tankstellen.js
+++ js/tankstellen.js
@@ -14,21 +14,21 @@ var tankstellen;
     }
     else{
       apiKey="00000000-0000-0000-0000-000000000002";
-      returnVal="Used DemoKey - Will only return test data";
+      returnVal="Using Demo key - will only return test data";
     }
     initialized=true;
     return returnVal;
   }
   function Search(data){
-    if(initialized){
+    if(initialized && data !== undefined){
       lat=data.latitude!==undefined?data.latitude:50.941278;
       lng=data.longitude!==undefined?data.longitude:6.958281;
       radius=data.apiKey!==undefined?data.radius:10;
       radius=radius>=25?25:radius;
       type=data.spritType!==undefined?data.spritType:"all";
       type=(type=="e5"||type=="e10"||type=="diesel"||type=="all")?type:"all";
-      order=data.sortBy!==undefined?data.sortBy:"dist";
-      order=(order=="price"||order=="dist")?order:"dist";    
+      order=data.sortBy!==undefined?data.sortBy:"price";
+      order=(order=="price"||order=="dist")?order:"price";    
       $.ajax({
         url:listPath,
         data:{
@@ -41,7 +41,8 @@ var tankstellen;
         },
         success: function(response){
           if(response.ok){
-            return response;
+            console.log(response.stations);
+            return response.stations;
           }
           else{
             console.log(response.message);
@@ -49,6 +50,31 @@ var tankstellen;
           }
         }
       });
+    }
+    else if(data == undefined){
+      return false;
+    }
+    else{
+      console.log("Not initialized.");
+      return false;
+    }
+  }
+  function prices(data){
+    if(initialized){
+      /*
+        TODO
+      */
+    }
+    else{
+      console.log("Not initialized.");
+      return false;
+    }
+  }
+  function detail(data){
+    if(initialized){
+      /*
+        TODO
+      */
     }
     else{
       console.log("Not initialized.");
@@ -58,4 +84,6 @@ var tankstellen;
   
   tankstellen.Init = Init;
   tankstellen.Finden = Search;
+  tankstellen.Preise = prices;
+  tankstellen.Details = detail;
 }( tankstellen || (tankstellen = {})));
